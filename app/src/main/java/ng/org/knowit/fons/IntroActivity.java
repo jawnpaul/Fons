b6a1a9096194546fb6aa825a103c67e97c28b969
John Paul <jpaul5246@gmail.com>
2019-06-17 01:11:44 +0100

    Playing around with app-intro library for onboarding purpose

---
+++ app/src/main/java/ng/org/knowit/fons/IntroActivity.java
@@ -0,0 +1,80 @@
+package ng.org.knowit.fons;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.github.paolorotolo.appintro.model.SliderPage;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setFadeAnimation();
+
+        /*setFadeAnimation();
+        setZoomAnimation();
+        setFlowAnimation();
+        setSlideOverAnimation();
+        setDepthAnimation();*/
+
+        // Instead of fragments, you can also use our default slide.
+        // Just create a `SliderPage` and provide title, description, background and image.
+        // AppIntro will do the rest.
+        SliderPage sliderPage = new SliderPage();
+        sliderPage.setTitle("I don't know which title");
+        sliderPage.setDescription("In here goes the description of the whatever");
+        sliderPage.setImageDrawable(R.drawable.app_installation);
+        sliderPage.setBgColor(Color.parseColor("#1976D2"));
+        addSlide(AppIntroFragment.newInstance(sliderPage));
+
+        SliderPage sliderPage2 = new SliderPage();
+        sliderPage2.setTitle("I don't know which title");
+        sliderPage2.setDescription("In here goes the description of the error mango");
+        sliderPage2.setImageDrawable(R.drawable.error);
+        sliderPage2.setBgColor(Color.parseColor("#1976D2"));
+        addSlide(AppIntroFragment.newInstance(sliderPage2));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(true);
+        setProgressButtonEnabled(true);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+        setVibrate(false);
+        setVibrateIntensity(30);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}
+
+
+
