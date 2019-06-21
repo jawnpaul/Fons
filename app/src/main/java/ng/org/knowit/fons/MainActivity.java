0c63fd3787e171fa8034195cc92aad07796507c2
John Paul <jpaul5246@gmail.com>
2019-06-21 21:55:53 +0100

    I think I have gotten the sync between onboarding and splash correctly now

--- app/src/main/java/ng/org/knowit/fons/MainActivity.java
+++ app/src/main/java/ng/org/knowit/fons/MainActivity.java
@@ -28,10 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
     Handler mHandler;
 
+    SharedPreferences mSharedPreferences;
+
+    Boolean isFirstStart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+        isFirstStart = mSharedPreferences.getBoolean("firstStart", true);
 
 
         //  Declare a new thread to do a preference check
@@ -39,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //  Initialize SharedPreferences
-                SharedPreferences getPrefs = PreferenceManager
-                        .getDefaultSharedPreferences(getBaseContext());
+                /*SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());*/
 
                 //  Create a new boolean and preference and set it to true
-                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+                /* boolean isFirstStart = getPrefs.getBoolean("firstStart", true);*/
 
                 //  If the activity has never started before...
                 if (isFirstStart) {
@@ -59,34 +67,45 @@ public class MainActivity extends AppCompatActivity {
                     });
 
                     //  Make a new preferences editor
-                    SharedPreferences.Editor e = getPrefs.edit();
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
 
                     //  Edit preference to make it false because we don't want this to run again
                     e.putBoolean("firstStart", false);
 
                     //  Apply changes
                     e.apply();
+
+
                 }
             }
         });
 
         // Start the thread
         t.start();
-
-
-        mHandler =new Handler();
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(intent);
-                finish();
-            }
-        },3000);
+        showSplash(isFirstStart);
 
 
 
     }
 
+
+    private void showSplash(boolean isFirstStart) {
+
+        if (isFirstStart) {
+            finish();
+        } else {
+            mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 3000);
+        }
+
+
+    }
 
 }
